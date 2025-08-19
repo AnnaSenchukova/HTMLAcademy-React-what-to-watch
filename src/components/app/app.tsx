3e35ae8cd385a2703a7ac99bbd3e2aed87782f06
anna <anna.senchukova@goods.ru>
2025-08-19 15:23:24 +0300

    task/2.10: добавлена разметка, html сконвертирован в tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,29 @@
 import {AddReview} from '../../pages/AddReview';
+import {HeadGuest} from '../../pages/HeadGuest';
+import {Main} from '../../pages/Main';
+import {MoviePage} from '../../pages/MoviePage';
+import {MoviePageDetails} from '../../pages/MoviePageDetails';
+import {MoviePageReviews} from '../../pages/MoviePageReviews';
+import {MoviePageInList} from '../../pages/MoviePageInList';
+import {MyList} from '../../pages/MyList';
+import {SignIn} from '../../pages/SignIn';
+import {SignInError} from '../../pages/SignInError';
+import {SignInMessage} from '../../pages/SignInMessage';
 
 function App(): JSX.Element {
   return (
     <body>
       <AddReview />
+      <HeadGuest />
+      <Main />
+      <MoviePage />
+      <MoviePageDetails />
+      <MoviePageReviews />
+      <MoviePageInList />
+      <MyList />
+      <SignIn />
+      <SignInError />
+      <SignInMessage />
     </body>
   );
 }
